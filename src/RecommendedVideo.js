8605ab20692512819ead572e2dbbd23e6be9c07a
Satyam <satyambhardawaj@gmail.com>
2020-11-19 14:16:10 +0530

    new features

--- src/RecommendedVideo.js
+++ src/RecommendedVideo.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './RecommendedVideo.css'
 import Video from './Video';
+import axios from 'axios';
 
 function RecommendedVideos() {
+
+    const youtube = axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBPlzrRv559Sh5UO-l5Z05KdYz8O6ZzX_g")
+    youtube.then((item) => {
+        console.log(item);
+    }).catch(e => console.log(e))
+
     return (
         <div className="recommendedVideo">
             <h1>RecommendedVideos</h1>
             <div className="recommendedVideo__videos">
                 <Video 
-                    title="Become a web developer"
+                    title="title"
                     views="2.3M views"
                     timestamp="3 days ago"
                     channelImage="https://instagram.fdel3-2.fna.fbcdn.net/v/t51.2885-19/s320x320/71186285_3021376311224610_5164292243195953152_n.jpg?_nc_ht=instagram.fdel3-2.fna.fbcdn.net&_nc_ohc=l_9qb2vngDsAX9kCuZB&_nc_tp=25&oh=2a7f1c9e2fdd8678515837394da8a4b1&oe=5FD70A69"
